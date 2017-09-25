4609b5c49f39c5455ef5dc9413f9c0fd06c28de3
Yaakushi <kaioaugusto.8@gmail.com>
2017-09-24 22:20:34 -0300

    code done

--- src/prox-subconj.cpp
+++ src/prox-subconj.cpp
@@ -7,11 +7,29 @@ using std::sort;
 
 void geraProximoConjunto(int *conjunto, int sizorig, int sizsub)
 {
-	
+	if(conjunto[sizsub-1] == sizorig)
+	{
+		if(sizsub == 1)
+		{
+			conjunto[0] = 1;
+			return;
+		}
+		//Caso o ultimo digito do prefixo atual seja o maior possivel, incrementa
+		//o prefixo menor antes.
+		geraProximoConjunto(conjunto, sizorig-1, sizsub-1);
+
+		//E entao volta o final do prefixo atual para o menor valor possivel.
+		conjunto[sizsub-1] = conjunto[sizsub-2] + 1;
+	}
+	else
+	{
+		// Caso contrario, apenas incrementa o final do prefixo atual.
+		conjunto[sizsub-1]++;
+	}
 }
 
 int main(int argc, char *argv[])
-{   
+{
 	if(argc < 3) 
 	{
 		cout << "Uso: " << argv[0] << " [tamanho conj. original] [tamanho"
@@ -43,14 +61,16 @@ int main(int argc, char *argv[])
 
 	// Como a definição do problema não especificou que a entrada estaria
 	// ordenada, sejamos um pouco cautelosos.
-	// Algoritmo da libc++ deveria checar se o vetor já está ordenado e não
+	// Caso o professor seja gentil e ja forneca a entrada ordenada, o 
+	// algoritmo da libc++ deveria checar se o vetor já está ordenado e não
 	// prejudicar (muito) a performance.
 	sort(conjunto, conjunto+sizsub);
 
-	for(int i = 0; i < sizsub; i++)
-		cout << conjunto[i] << " ";
-	
-	cout << endl;
-
 	geraProximoConjunto(conjunto, sizorig, sizsub);
+
+	for(int i = 0; i < sizsub; i++)
+	{
+		cout << conjunto[i] << (i == (sizsub - 1) ? "" : " " );
+	}
+	cout << endl;
 }
